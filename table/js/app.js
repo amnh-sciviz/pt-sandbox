a3792aceff0c84a3080346ab6efc9c516c04bbef
Brian Foo <brian@youaremyjoy.org>
2019-06-05 16:24:45 -0400

    add abundance view to periodic table

--- table/js/app.js
+++ table/js/app.js
@@ -3,14 +3,15 @@
 var TableApp = (function() {
 
   var app, opt, $canvas, w, h;
-  var elementData;
+  var elementData, currentView;
 
   function TableApp(config) {
     var defaults = {
       el: '#canvas',
       configUrl: 'config.json',
       contentUrl: 'content.json',
-      dataUrl: 'data/elements.csv'
+      dataUrl: 'data/elements.csv',
+      baseScaleFactor: 5.0, // for abundance, 100% = x the size (e.g. 10x)
     };
     opt = _.extend({}, defaults, config);
 
@@ -37,6 +38,7 @@ var TableApp = (function() {
     $canvas = $(opt.el);
     w = $canvas.width();
     h = $canvas.height();
+    currentView = 'default';
 
     $.when(
       $.getJSON(opt.configUrl),
@@ -49,16 +51,30 @@ var TableApp = (function() {
       console.log('Config and content loaded.');
       elementData = app.parseData(data);
       app.loadUI(elementData);
+      app.loadListeners();
     });
   };
 
-  TableApp.prototype.loadListeners = function(){
+  TableApp.prototype.changeView = function(name){
+    var $active = $('.button-view[data-view="'+name+'"]');
+    if ($active.hasClass('active')) return;
 
+    $('.button-view').removeClass('active');
+    $active.addClass('active');
+
+    if (name==="abundance") this.showViewAbundance();
+    else this.showViewDefault();
+  };
+
+  TableApp.prototype.loadListeners = function(){
+    $('.button-view').on('click', function(e){
+      app.changeView($(this).attr('data-view'), $(this));
+    });
   };
 
   TableApp.prototype.loadUI = function(data){
     var $table = $('<div class="table"></div>');
-    _.each(data, function(d){
+    _.each(data, function(d, i){
       var $el = $('<div id="element-'+d.Symbol+'" class="'+d.ClassName+'"></div>');
       var $a = $('<a href="#'+d.Symbol+'" data-index="'+d.index+'"></a>');
       $a.append('<span class="number">'+d.AtomicNumber+'</span>');
@@ -66,6 +82,7 @@ var TableApp = (function() {
       $a.append('<span class="name">'+d.Element+'</span>');
       $el.append($a);
       $table.append($el);
+      elementData[i].$el = $el;
     });
     $table.append('<div class="element placeholder row-5 col-2"><div><span>57-71</span></div></div>');
     $table.append('<div class="element placeholder row-6 col-2"><div><span>89-103</span></div></div>');
@@ -84,6 +101,20 @@ var TableApp = (function() {
       return d;
     });
     return parsedData;
+  };
+
+  TableApp.prototype.showViewAbundance = function(){
+    _.each(elementData, function(d){
+      var factor = opt.baseScaleFactor * Math.sqrt(d.Abundance);
+      factor = Math.max(factor, 0.2);
+      d.$el.css({'transform': 'scale3d('+factor+','+factor+','+factor+')', 'opacity': 0.8});
+    });
+  };
+
+  TableApp.prototype.showViewDefault = function(){
+    _.each(elementData, function(d){
+      d.$el.css({'transform': '', 'opacity': 1.0});
+    });
   };
 
   return TableApp;
