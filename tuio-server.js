83479172bc57b62950fdd144f9802da269fe3034
beefoo <brian@youaremyjoy.org>
2019-06-11 12:16:13 -0400

    fix errors with tuio server

--- tuio-server.js
+++ tuio-server.js
@@ -1,24 +1,25 @@
 // > node server.js 8888
-var port = 2222;
+var port = 8080;
 if (process.argv.length > 2) port = parseInt(process.argv[2]);
 
 const dServer = require('dgram').createSocket('udp4');
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+const http = require('http');
 const WebSocket = require('ws');
 
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // bind UDP datagram server to standard TUIO port
 dServer.bind(3333, '0.0.0.0');
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function(ws) {
   console.log('Connected to WebSocket Server');
-  
+
   // forward UDP packets via WebSockets
   dServer.on('message', function(buf, rinfo){
-    ws.send({buffer: buf, sender: rinfo});
+    ws.send(buf);
   });
 
   dServer.on('listening', function() {
